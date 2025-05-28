7898509f68ddaf29a80e431191ae9d83e4610cbb
GiannisKarakousis <ikarakousis@hotmail.com>
2025-05-28 10:22:57 +0300

    Initial commit

---
+++ journalist-media-manager-springboot/src/main/java/com/example/journalist_media_manager_springboot/JournalistMediaManagerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.journalist_media_manager_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalistMediaManagerSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalistMediaManagerSpringbootApplication.class, args);
+	}
+
+}
---
+++ journalist-media-manager-springboot/src/test/java/com/example/journalist_media_manager_springboot/JournalistMediaManagerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.journalist_media_manager_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalistMediaManagerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
